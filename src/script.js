afb23a45638cf3550ed4f2114888effaef8cde43
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2023-02-16 18:02:33 +0100

    update

--- src/script.js
+++ src/script.js
@@ -1,12 +1,94 @@
 const display = document.querySelector ("#display");
 const numberButton = document.querySelectorAll(".number");    
-const operator= document.querySelectorAll(".operator");
+const operatorButton= document.querySelectorAll(".operator");
 const goBack =document.querySelector("#backspace");
 const equal =document.querySelector("#result");
 let currentNumber ;
+let currentOperator;
+let firstNumber;
+let secondNumber;
+
+
+
+const operation = (operator, a, b) => {
+    a = Number(a);
+    b= Number(b);
+    switch(operator) {
+        case " +":
+        return addition( a,b);
+        case " - ":
+        return substracting (a,b);
+        case " * " :
+        return multiplication(a,b);
+        case " / ":
+        return divide (a,b); 
+
+    }
+};
+
+
+function updateDisplay (){
+    display.value =currentNumber;    
+}
+
+
+function buttonNumberClicked(event){  
+   
+   if (currentNumber == null){
+    currentNumber= event.target.innerText;
+   } else {
+    newSelectedNumber = event.target.innerText;
+    newNumber = currentNumber.toString() + newSelectedNumber.toString();
+    currentNumber= parseInt(newNumber);
+    console.log(currentNumber);
+   }
+
+   updateDisplay();
+}
+
+for (i=0; i < numberButton.length; i++){
+    numberButton[i].addEventListener('click', buttonNumberClicked);
+}
+
+
+function operationButtonClicked (event){
+    if (currentNumber) {
+        currentOperator = event.target.innerText;
+        console.log(currentOperator);
+        if(firstNumber){
+            secondNumber = currentNumber;
+            currentNumber = operation(currentOperator, firstNumber, secondNumber);
+            updateDisplay();
+        }else {
+            firstNumber =currentNumber;
+        }  
+        currentNumber =null;
+    }
+}
+
+for (i=0; i < operatorButton.length; i++){
+    operatorButton[i].addEventListener('click', operationButtonClicked);
+}
+
+
+
+
+function clearInput() {
+    let clearAll =document.querySelector("#display");
+    if (clearAll.value != " ") {
+        clearAll.value ="";
+        currentNumber = null;
+  }
+
+  clearAll.addEventListener('click', clearInput);
+}
+
 
 function addition (a, b) {
-   return a + b;
+    console.log(a,b);
+    console.log(a +b);
+    return a + b;
+
 }
 
 function substracting(a, b){
@@ -18,43 +100,13 @@ function multiplication (a,b ){
 }
 
 function divide(a, b){
-    return a /b;
+    if (b === 0) {
+        return ("No division by 0")
+    }
+    return a /b 
+  
 }   
 
-function updateDisplay (){
-    display.value =currentNumber;    
-}
 
-
-function buttonClicked(event){  
-   
-   if (currentNumber == null){
-    currentNumber= event.target.innerText;
-   } else {
-    newSelectedNumber = event.target.innerText;
-    newNumber = currentNumber.toString() + newSelectedNumber.toString();
-    currentNumber= parseInt(newNumber);
-    console.log(currentNumber);
-
-   }
-
-   updateDisplay();
-}
-
-for (i=0; i < numberButton.length; i++){
-    numberButton[i].addEventListener('click', buttonClicked);
-    
-
-}
-
-function clearInput() {
-    let clearAll =document.querySelector("#display");
-    console.log(clearAll);
-    if (clearAll.value != " ") {
-        clearAll.value ="";
-  }
-
-  clearAll.addEventListener('click', clearInput);
-}
 
 
